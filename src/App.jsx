e7316b2076634b05a6b63a1af047890137cc9c32
Anastasiia Totska <nastya.totskaya1997@gmail.com>
2025-04-06 14:38:09 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
-import { useState, useEffect } from "react";
-import Description from "./components/ContactForm/ContactForm"
-import Feedback from "./components/SearchBox/SearchBox";
-import Options from "./components/Contact/Contact";
-import Notification from "./components/ContactList/ContactList";
+import Contact from "./components/Contact/Contact"
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 const App = () => {
 
   return (
     <>
-      <Description />
-      <Options/>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+      <Contact />
     </>
   );
 }
